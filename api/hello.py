81bb0b59e0ffe488491cb6b409789ead0c5a4b2c
coji <coji@techtalk.jp>
2023-08-17 22:33:59 +0900

    feat: メール送信とThank you ページ表示を実装

--- api/hello.py
+++ api/hello.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import os
 import resend
-from fastapi.responses import RedirectResponse
+from fastapi.responses import HTMLResponse
 
 resend.api_key = os.environ["RESEND_API_KEY"]
 
@@ -10,17 +10,27 @@ app = FastAPI()
 
 @app.get("/api/hello")
 async def read_root(name: str, email: str, message: str):
-    params = {
-        "from": "kouheihand@yahoo.co.jp",
-        "to": [email],
-        "subject": "hello world",
-        "html": "<strong>it works!</strong>",
-    }
-    result = "できたよ"  # await resend.Emails.send(params)
-    # return {
+    # メール送信
+    result = resend.Emails.send(
+        {
+            "from": "onboarding@resend.dev",  # テスト用: ドメイン認証が不要な resend.dev ドメインで送信
+            "to": [email],
+            "subject": "お問い合わせありがとうございます",
+            "html": f"<strong>{message}</strong>",
+        }
+    )
+
+    # thank you の HTML ファイルの内容を読み込む
+    with open("./thankyou_contact.html") as f:
+        html = f.read()
+
+    # TODO: 読み込んだHTMLの中の文字列を置換する
+    # {
     #     "name": name,
     #     "email": email,
     #     "message": message,
     #     "result": result,
     # }
-    return RedirectResponse("../index.html")
+
+    # HTML を返す
+    return HTMLResponse(content=html)
