b3fb6f528742dcfc64111bf8051a2c0872d10074
koheis <kouheihand@yahoo.co.jp>
2023-08-17 17:38:03 +0900

    問い合わせフォーム登録したらindex.htmlへRedirect

--- api/hello.py
+++ api/hello.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+import os
+import resend
+from fastapi.responses import RedirectResponse
+
+resend.api_key = os.environ["RESEND_API_KEY"]
 
 app = FastAPI()
 
 
 @app.get("/api/hello")
 async def read_root(name: str, email: str, message: str):
-    return {"name": name, "email": email, "message": message}
+    params = {
+        "from": "kouheihand@yahoo.co.jp",
+        "to": [email],
+        "subject": "hello world",
+        "html": "<strong>it works!</strong>",
+    }
+    result = "できたよ"  # await resend.Emails.send(params)
+    # return {
+    #     "name": name,
+    #     "email": email,
+    #     "message": message,
+    #     "result": result,
+    # }
+    return RedirectResponse("../index.html")
